5cfc1bfb3ba53156f886e0c6af059a110ab52192
Val <vwong031@ucr.edu>
2022-07-13 22:44:41 -0700

    Base code for welcome message & printing main menu

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void printMenu();
+
+int main() {
+  cout << "Hi. Welcome to the Restaurant Database! Please choose an option from the menu" << endl;
+
+  printMenu();
+
+  return 0;
+}
+
+void printMenu() {
+  int userChoice;
+
+  cout << "OPTIONS:" << endl;
+  cout << "0 - Enter restaurant name" << endl
+    << "1 - Enter rating" << endl
+    << "2 - Enter cuisine" << endl
+    << "3 - Write a review" << endl << endl
+    << "Choose an option: ";
+
+  cin >> userChoice;
+  cin.ignore();
+
+  if (userChoice == 0) {
+    cout << "ENTERING RESTAURANT NAME" << endl;
+  }
+  else if (userChoice == 1) {
+    cout << "ENTERING RATING" << endl;
+  }
+  else if (userChoice == 2) {
+    cout << "ENTERING CUISINE" << endl;
+  }
+  else if (userChoice == 3) {
+    cout << "ENTERING REVIEW" << endl;
+  }
+  else {
+    cout << "Invalid input. Choose another option." << endl << endl;
+  }
+    
+}
